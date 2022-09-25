afe401a4601b9798eff702a03953a4cd6eea7b30
miadahlgren <101955676+miadahlgren@users.noreply.github.com>
2022-09-25 11:16:51 +0200

    minor changes in positioning and fixed the overlay images and to get the artist in same row instead of different paragraphs

--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Artist = (props) => {
   return (
     <>
+    <div className='artistNameDiv'>
         {props.data.artists.map((artist, index) => {
           return (
             <a className="artist-link"
@@ -10,14 +11,14 @@ const Artist = (props) => {
               href={artist.external_urls.spotify}
               target="_blank" rel="noopener noreferrer">
 
-                <p className='artistNameP'>
-                  {artist.name}
-                  {props.data.artists.length - 1 > index ? ', ' : ''}
-                </p>
+               
+                  {artist.name}{props.data.artists.length - 1> index ? ', ' : ''}
+              
             </a>
           )
         })
         }
+        </div>
     </>
   );
 }
